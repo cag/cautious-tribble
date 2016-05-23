421fac5c291817eb7eb176ac9c5dea67671b0beb
Alan Lu <gotoalanlu@gmail.com>
2016-05-23 18:52:55 -0500

    AHHhhH They're here to... be benign??

--- app/app.js
+++ app/app.js
@@ -1,4 +1,10 @@
-import LruSet from "collections/lru-set"
+import "babel-polyfill";
+import List from 'collections/list'
+import LruSet from "collections/lru-set";
+
+for(let col_cls of [List, LruSet]) {
+    col_cls.prototype[Symbol.iterator] = col_cls.prototype.iterate
+}
 
 let canvas;
 let ctx;
@@ -8,17 +14,24 @@ let timestamp;
 let mouse = {
     x: 0,
     y: 0
+};
+
+function clampCoordinates(entity) {
+    entity.x = Math.min(Math.max(entity.x, -1), 17);
+    entity.y = Math.min(Math.max(entity.y, -1), 10);
 }
+
 let player = {
     x: 8,
     y: 4.5,
     dirx: 1,
     diry: 0,
     speed: .005,
-    bullets: LruSet([], 15),
+    bullets: LruSet([], 50),
     bullet_timer: 0,
     bullet_interval: 100,
     bullet_speed: .01,
+
     draw: function() {
         ctx.save();
         ctx.transform(
@@ -27,16 +40,15 @@ let player = {
             -player.diry,
             player.dirx,
             player.x, player.y);
-        ctx.fillStyle = '#f00';
         ctx.beginPath();
         ctx.moveTo(.3, 0);
         ctx.lineTo(-.25, .25);
         ctx.lineTo(-.25, -.25);
         ctx.closePath();
-        ctx.fill();
+        ctx.stroke();
         ctx.restore();
 
-        player.bullets.forEach((bullet) => {
+        for(let bullet of player.bullets) {
             ctx.save();
             ctx.transform(
                 bullet.dirx,
@@ -46,11 +58,11 @@ let player = {
                 bullet.x, bullet.y);
             ctx.fillStyle = '#00f';
             ctx.beginPath();
-            ctx.fillRect(-.1, -.05, .2, .1);
+            ctx.fillRect(-.07, -.07, .07, .07);
             ctx.closePath();
             ctx.fill();
             ctx.restore();
-        });
+        };
     },
     update: function(dt) {
         let dx = 0,
@@ -69,11 +81,21 @@ let player = {
             player.bullet_timer -= player.bullet_interval;
         }
 
-        player.bullets.forEach((bullet) => {
+        for(let bullet of player.bullets) {
             let bds = player.bullet_speed * dt;
             bullet.x += bullet.dirx * bds;
             bullet.y += bullet.diry * bds;
-        });
+            if(bullet.x < -1 || bullet.x > 17 || bullet.y < -1 || bullet.y > 10) {
+                player.bullets.delete(bullet);
+            } else {
+                for(let enemy of enemies) {
+                    if(enemy.containsPoint(bullet.x, bullet.y)) {
+                        enemy.takeDamage();
+                        player.bullets.delete(bullet);
+                    }
+                }
+            }
+        };
 
         if(pressed[37] || pressed[65])
             dx -= ds;
@@ -92,6 +114,7 @@ let player = {
         }
         player.x += dx;
         player.y += dy;
+        clampCoordinates(player);
 
         let mdx = mouse.x - player.x,
             mdy = mouse.y - player.y,
@@ -102,17 +125,94 @@ let player = {
     }
 };
 
-export function onresize() {
-    let cw = window.innerWidth;
-    let ch = window.innerHeight - 4;
-    if(cw / ch > 16/9) cw = ch * 16/9;
-    else ch = cw * 9/16;
-    canvas.width = cw;
-    canvas.height = ch;
-    unit = ch / 9;
+let enemies = List([]);
+let enemy_bullets = LruSet([], 50);
+
+class Mine {
+    constructor(obj) {
+        enemies.push(this);
+        this.node = enemies.head.prev;
+        Object.assign(this, {
+            x: 0,
+            y: 0,
+            speed: .002,
+            dirx: 1,
+            diry: 0,
+            radius: .125,
+            hp: 3,
+            tracksPlayer: true
+        });
+        Object.assign(this, obj);
+    }
+
+    draw() {
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fillStyle = this.hp == 3 ? '#000' :
+            this.hp == 2 ? '#800' : '#f00';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius/2, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.restore();
+    }
+
+    update(dt) {
+        if(this.tracksPlayer) {
+            let dx = player.x - this.x,
+                dy = player.y - this.y,
+                ds = Math.sqrt(dx*dx+dy*dy);
+            this.dirx = dx/ds;
+            this.diry = dy/ds;
+        } else {
+            if(this.x == -1 || this.x == 17) {
+                this.dirx = -this.dirx
+            }
+            if(this.y == -1 || this.y == 10) {
+                this.diry = -this.diry
+            }
+        }
+        this.x += this.dirx * this.speed * dt;
+        this.y += this.diry * this.speed * dt;
+        clampCoordinates(this);
+    }
+
+    containsPoint(x, y) {
+        let dx = x - this.x,
+            dy = y - this.y;
+        return dx*dx+dy*dy<this.radius*this.radius;
+    }
+
+    takeDamage() {
+        this.hp--;
+        if(this.hp <= 0)
+            this.node.delete();
+    }
+
+    static create1(x, y, theta) {
+        return new Mine({
+            x: x,
+            y: y,
+            dirx: Math.cos(theta),
+            diry: Math.sin(theta)
+        });
+    }
 }
 
+
 export function run() {
+    let x, y, theta;
+    for([x, y, theta] of [
+        [1, 1, Math.PI/4],
+        [15, 1, Math.PI/4],
+        [1, 5, Math.PI/4],
+        [15, 4, Math.PI/4],
+        [15, 8, Math.PI/4],        
+    ]) {
+        Mine.create1(x, y, theta);
+    }
+
     canvas = document.getElementById('app');
     document.body.onkeydown=function(e){
          e = e || window.event;
@@ -122,6 +222,7 @@ export function run() {
          e = e || window.event;
          delete pressed[e.keyCode];
     };
+
     let onresize=function() {
         if(canvas) {
             let cw = window.innerWidth;
@@ -133,7 +234,7 @@ export function run() {
             unit = ch / 9;
         }
     };
-    document.body.onresize = onresize;
+    window.addEventListener('resize', onresize);
     onresize();
 
     document.body.onmousemove = function (evt) {
@@ -151,12 +252,16 @@ export function run() {
 function draw() {
     ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.lineWidth=.02;
     // ctx.fillText(timestamp, 10, 10);
 
     ctx.save();
     ctx.scale(unit, unit);
     ctx.fillRect(mouse.x-.1, mouse.y-.1, .2, .2);
 
+    for(let enemy of enemies) {
+        enemy.draw();
+    }
     player.draw();
 
     ctx.restore();
@@ -167,6 +272,9 @@ function update() {
     let dt = performance.now() - timestamp;
     timestamp = performance.now();
     
+    for(let enemy of enemies) {
+        enemy.update(dt);
+    }
     player.update(dt);
 
     setTimeout(update, 1000/60);
